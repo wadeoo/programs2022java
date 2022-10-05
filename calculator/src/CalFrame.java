374f037b91e4678025dd921e080bd7e1a8876f63
WADEWU <wwaaddeewwuu@gmail.com>
2022-10-06 00:55:24 +0800

    change

--- calculator/src/CalFrame.java
+++ calculator/src/CalFrame.java
@@ -1,3 +1,5 @@
+import com.sun.xml.internal.ws.util.StringUtils;
+
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
@@ -8,7 +10,7 @@ import java.awt.event.ActionListener;
 
 public class CalFrame extends JFrame implements ActionListener {
     private final JLabel resultLabel;
-    private final JButton btn7,btn8,btn9,btnMulti,btnEqual,btnAdd,btnSubt
+    private final JButton btn7,btn8,btn9,btn4,btn5,btn6,btn1,btn2,btn3,btn0,btnMulti,btnEqual,btnAdd,btnSubt
             ,btnDiv,btnDot,btnBack,btnClear;
     private  final  Dimension btnDim=new Dimension(70,40);
     private  String operand1="", operand2="";//运算子
@@ -18,6 +20,10 @@ public class CalFrame extends JFrame implements ActionListener {
     private ScriptEngineManager mgr =new ScriptEngineManager();
     private ScriptEngine engine =mgr.getEngineByName("JavaScript");
 
+
+    //regex
+    String OperatorAndDotMatch =".*[\\+\\-*/.]$";
+    String IllegalDotUsePreventMatch=".*[0-9]*\\.[0-9]*$";
 
     public CalFrame() throws HeadlessException {
 
@@ -38,9 +44,7 @@ public class CalFrame extends JFrame implements ActionListener {
         //getContentPane().setBackground(new Color(1.0f,1.0f,1.0f,0.0f));
 
 
-
-
-        resultLabel = new JLabel("0",JLabel.RIGHT);
+        resultLabel = new JLabel(operation,JLabel.RIGHT);
         resultLabel.setSize(295,40);
         resultLabel.setLocation(5,5);
         Font resultFont = new Font("黑体",Font.BOLD,30);
@@ -90,6 +94,62 @@ public class CalFrame extends JFrame implements ActionListener {
         btn9.setFont(resultFont);
         btn9.addActionListener(this);
         add(btn9);
+
+        //4
+        btn4 = new JButton("4");
+        btn4.setLocation(5,140);
+        btn4.setSize(btnDim);
+        btn4.setFont(resultFont);
+        btn4.addActionListener(this);
+        add(btn4);
+
+        //5
+        btn5 = new JButton("5");
+        btn5.setLocation(80,140);
+        btn5.setSize(btnDim);
+        btn5.setFont(resultFont);
+        btn5.addActionListener(this);
+        add(btn5);
+
+        //6
+        btn6 = new JButton("6");
+        btn6.setLocation(155,140);
+        btn6.setSize(btnDim);
+        btn6.setFont(resultFont);
+        btn6.addActionListener(this);
+        add(btn6);
+
+        //1
+        btn1 = new JButton("1");
+        btn1.setLocation(5,185);
+        btn1.setSize(btnDim);
+        btn1.setFont(resultFont);
+        btn1.addActionListener(this);
+        add(btn1);
+
+        //2
+        btn2 = new JButton("2");
+        btn2.setLocation(80,185);
+        btn2.setSize(btnDim);
+        btn2.setFont(resultFont);
+        btn2.addActionListener(this);
+        add(btn2);
+
+        //3
+        btn3 = new JButton("3");
+        btn3.setLocation(155,185);
+        btn3.setSize(btnDim);
+        btn3.setFont(resultFont);
+        btn3.addActionListener(this);
+        add(btn3);
+
+        //0
+        btn0 = new JButton("0");
+        btn0.setLocation(80,230);
+        btn0.setSize(btnDim);
+        btn0.setFont(resultFont);
+        btn0.addActionListener(this);
+        add(btn0);
 
         //除法
         btnDiv= new JButton("÷");
@@ -144,37 +204,7 @@ public class CalFrame extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        /*if(e.getSource()==btn7) {
-            if(operatorType==0) {
-                operand1+= "7";
-                resultLabel.setText(operand1);
-            }else {
-               operand2+="7" ;
-               resultLabel.setText(operand2);
-            }
-        }else if(e.getSource()==btn8){
-            if(operatorType==0) {
-                operand1 += "8";
-                resultLabel.setText(operand1);
-            }else {
-                operand2+="8";
-                resultLabel.setText((operand2));
-            }
-        }else if(e.getSource()==btnMulti){
-            operatorType=3;
-        }else if(e.getSource()==btnEqual){
-            float foperand1= Float.parseFloat(operand1);
-            float foperand2= Float.parseFloat(operand2);
-            if(operatorType==3){
-                float fresult=foperand1*foperand2;
-                sresult=Float.toString(fresult);
-                resultLabel.setText(sresult);
-                operatorType=0;
-            }
-        }*/
 
-
-        //
         if(e.getSource()==btn7){
             if(operation=="0"){
                 operation="";
@@ -187,42 +217,87 @@ public class CalFrame extends JFrame implements ActionListener {
             }
             operation+="8";
             resultLabel.setText(operation.replace("/","÷"));
+        }else if(e.getSource()==btn9){
+            if(operation=="0"){
+                operation="";
+            }
+            operation+="9";
+            resultLabel.setText(operation.replace("/","÷"));
+        }else if(e.getSource()==btn4){
+            if(operation=="0"){
+                operation="";
+            }
+            operation+="4";
+            resultLabel.setText(operation.replace("/","÷"));
+        }else if(e.getSource()==btn5){
+            if(operation=="0"){
+                operation="";
+            }
+            operation+="5";
+            resultLabel.setText(operation.replace("/","÷"));
+        }else if(e.getSource()==btn6){
+            if(operation=="0"){
+                operation="";
+            }
+            operation+="6";
+            resultLabel.setText(operation.replace("/","÷"));
+        }else if(e.getSource()==btn1){
+            if(operation=="0"){
+                operation="";
+            }
+            operation+="1";
+            resultLabel.setText(operation.replace("/","÷"));
+        }else if(e.getSource()==btn2){
+            if(operation=="0"){
+                operation="";
+            }
+            operation+="2";
+            resultLabel.setText(operation.replace("/","÷"));
+        }else if(e.getSource()==btn3){
+            if(operation=="0"){
+                operation="";
+            }
+            operation+="3";
+            resultLabel.setText(operation.replace("/","÷"));
+        }else if(e.getSource()==btn0){
+            if(operation!="0"){
+                operation+="0";
+                resultLabel.setText(operation.replace("/","÷"));
+            }
         }else if(e.getSource()==btnDiv){
-           if(!operation.matches(".*[\\+\\-*/.]$")){
+           if(!operation.matches(OperatorAndDotMatch)){
                 operation+="/";
                 resultLabel.setText(operation.replace("/","÷"));
             }
         }else if(e.getSource()==btnMulti){
-            if(!operation.matches(".*[\\+\\-*/.]$")){
+            if(!operation.matches(OperatorAndDotMatch)){
                 operation+="*";
                 resultLabel.setText(operation.replace("/","÷"));
             }
 
         }else if(e.getSource()==btnAdd){
-            if(!operation.matches(".*[\\+\\-*/.]$")){
+            if(!operation.matches(OperatorAndDotMatch)){
                 operation+="+";
                 resultLabel.setText(operation.replace("/","÷"));
             }
         }else if (e.getSource()==btnSubt) {
-            if(!operation.matches(".*[\\+\\-*/.]$")){
+            if(!operation.matches(OperatorAndDotMatch)){
                 operation+="-";
                 resultLabel.setText(operation.replace("/","÷"));
             }
         }else if(e.getSource()==btnDot){
-//            if(!(operation.endsWith("+")||operation.endsWith("-")
-//            ||operation.endsWith("*")||operation.endsWith("/")||operation.endsWith("."))){
-//                operation+=".";
-//                resultLabel.setText(operation.replace("/","÷"));
-//            }
-            if(!operation.matches(".*[\\+\\-*/.]$")&&!operation.matches(".*[0-9]*\\.[0-9]*$")){
+            if(!operation.matches(OperatorAndDotMatch)&&!operation.matches(IllegalDotUsePreventMatch)){
                 operation+=".";
                 resultLabel.setText(operation.replace("/","÷"));
             }
-
         }else if (e.getSource()==btnBack){
-            if(operation!="0")
-            operation=operation.substring(0,operation.length()-1);
-            resultLabel.setText(operation.replace("/","÷"));
+            if(operation!="0"){
+                operation= operation.replaceAll(".$","");
+                if(operation.equals("")){
+                    operation="0";
+                }
+                resultLabel.setText(operation.replace("/","÷"));
+            }
         }else if(e.getSource()==btnClear){
             operation="0";
             resultLabel.setText(operation.replace("/","÷"));
--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
    public static void main(String args[]) {
-      //System.out.println("Hello World");
-
+      String a ="9";
+      System.out.println(a.substring(0,a.length()-1));
       CalFrame calFrame = new CalFrame();//声明一个窗口对象,并初始化
       calFrame.show();//显示窗口
+
+
    }
 }
