917872804da124c931e35f8b7aa8ba7b988bca65
WADEWU <wwaaddeewwuu@gmail.com>
2022-10-17 11:38:43 +0800

    c

--- MYPS/src/cn/edu/fzu/sm2020/feiying/myps/MainFrame.java
+++ MYPS/src/cn/edu/fzu/sm2020/feiying/myps/MainFrame.java
@@ -32,6 +32,7 @@ public class MainFrame extends JFrame implements WindowStateListener {
             File file =fileChooser.getSelectedFile();
             if(file.exists()){
                 ii =new ImageIcon(file.getPath());
+
                 originImgLabel.setIcon(ii);
             }else {
                 JOptionPane.showMessageDialog(fileChooser,"文件不存在");
@@ -41,6 +42,16 @@ public class MainFrame extends JFrame implements WindowStateListener {
         }
     }
 
+    //inner class
+    public class HandleEdgeDetect implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(ii==null||ii.equals("")){
+                JOptionPane.showMessageDialog(MainFrame.this,"请先打开图像");
+            }
+        }
+    }
 
 
     public MainFrame() throws HeadlessException {
@@ -120,16 +131,22 @@ public class MainFrame extends JFrame implements WindowStateListener {
 
     private void toolInit() {
         JToolBar toolBar=new JToolBar();
+
         JButton openBtn=new JButton("打开图像");
+        JButton edgeDetectBtn=new JButton("边缘检测");
 
-        ImageIcon imageIcon=new ImageIcon("././img/folder.png");
-        imageIcon =new ImageIcon(imageIcon.getImage().getScaledInstance(20,20, Image.SCALE_SMOOTH));
+        ImageIcon imageIcon1=new ImageIcon("././img/folder.png");
+        imageIcon1 =new ImageIcon(imageIcon1.getImage().getScaledInstance(20,20, Image.SCALE_SMOOTH));
+        ImageIcon imageIcon2=new ImageIcon("././img/edge_detect.png");
+        imageIcon2 =new ImageIcon(imageIcon2.getImage().getScaledInstance(20,20, Image.SCALE_SMOOTH));
 
-
-        openBtn.setIcon(imageIcon);
+        openBtn.setIcon(imageIcon1);
         openBtn.addActionListener(new HandleOpen());
+        edgeDetectBtn.setIcon(imageIcon2);
+        edgeDetectBtn.addActionListener(new HandleEdgeDetect());
 
         toolBar.add(openBtn);
+        toolBar.add(edgeDetectBtn);
 
         this.add(toolBar,BorderLayout.NORTH);
 
@@ -138,13 +155,21 @@ public class MainFrame extends JFrame implements WindowStateListener {
 
     private void menuInit() {
         JMenuBar menuBar=new JMenuBar();
-        JMenu menu=new JMenu("文件");
-        JMenuItem iOpen=new JMenuItem("打开图像");
 
-        menu.add(iOpen);
-        iOpen.addActionListener(new HandleOpen());
-        menuBar.add(menu);
+        JMenu fileMenu=new JMenu("文件");
+        JMenu imgProcessMenu= new JMenu("图像处理");
 
+        JMenuItem iOpenItem=new JMenuItem("打开图像");
+        JMenuItem edgeDetectItem=new JMenuItem("边缘检测");
+
+        fileMenu.add(iOpenItem);
+        imgProcessMenu.add(edgeDetectItem);
+
+        menuBar.add(fileMenu);
+        menuBar.add(imgProcessMenu);
+
+        iOpenItem.addActionListener(new HandleOpen());
+        edgeDetectItem.addActionListener(new HandleEdgeDetect());
 
         this.setJMenuBar(menuBar);
     }
